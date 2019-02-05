949f81eb211c17de42b2e8b28ee93a168101ea40
nirvik00 <nirviksaha@gatech.edu>
2019-02-05 00:23:40 -0500

    phase 1 complete

--- public/js/nsGui.js
+++ public/js/nsGui.js
@@ -13,7 +13,6 @@ var guiControls=new function(){
   this.autoLoop=false;
   //this.setWireframe=true;
   this.minHt=3;
-  this.midHt=7;
   this.maxHt=20;
   this.ratio_Res_Comm=0.3;
   this.ratio_Comm_Off=0.3;
@@ -28,7 +27,6 @@ datgui.add(guiControls, "FSROffice", 0.1,3);
 datgui.add(guiControls, "autoLoop");
 //datgui.add(guiControls, "setWireframe");
 datgui.add(guiControls, "minHt",1,5);
-datgui.add(guiControls, "midHt",5,12);
 datgui.add(guiControls, "maxHt",12,25);
 
 var customContainer = document.getElementById('moveGUI');
