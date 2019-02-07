568a8165daff668d36b9430da893ffc0103deab6
nirvik00 <nirviksaha@gatech.edu>
2019-02-07 00:11:46 -0500

    guicomplete

--- public/js/nsGui.js
+++ public/js/nsGui.js
@@ -1,127 +1,71 @@
-/* GUI variables
-* gridLe
-* gridWi
-* gridHi
-*/
+
 /*
-var datgui= new dat.GUI({ autoPlace: false });
-var guiControls=new function(){
-  this.numL=2;
-  this.numH=2;
-  this.gridL=2.5;
-  this.gridH=2.5;
-  
-  this.evacuation_density=0.05;
-  
-  this.ratio_Road=0.25;
-  this.ratio_Path=0.50;
+var datgui=new dat.GUI({ autoPlace: false });
+//cell-grid gui controls
+var cellGuiControls=new function(){
+  this.num_Length=2;
+  this.num_Depth=2;
+  this.grid_Length=2.5;
+  this.grid_Depth=2.5;
+}
+var cellGUI=datgui.addFolder('cellGuiControls');
+cellGUI.add(cellGuiControls, "num_Length", 1, 5);
+cellGUI.add(cellGuiControls, "num_Depth", 1, 5);
+cellGUI.add(cellGuiControls, "grid_Length", 1, 5);
+cellGUI.add(cellGuiControls, "grid_Depth", 1, 5);
+
+//ground gui controls
+var groundGuiControls=new function(){
   this.ratio_Green=0.25;
-  
-  this.FSRRes=0.3;
-  this.FSRComm=0.3;
-  this.FSROffice=0.3;
-  
-  this.minHt=3;
-  this.midHt=7;
-  this.maxHt=20;
-  
-  this.ratio_Res_Comm=0.3;
-  this.ratio_Comm_Off=0.3;
-  this.ratio_Off_Res=0.3;
-  
-  this.autoLoop=false;
-  
-  this.show_OnlyGround=false;
-  
-  this.show_Road=true;
+  this.ratio_Path=0.50;
+  this.ratio_Road=0.25;
   this.show_Green=true;
   this.show_Path=true;
-  
+  this.show_Road=true;
+  this.show_Only_Ground=false;
+}
+var groundGUI=datgui.addFolder('groundGuiControls');
+groundGUI.add(groundGuiControls, "ratio_Green", 0.1,1);
+groundGUI.add(groundGuiControls, "ratio_Path", 0.1,1);
+groundGUI.add(groundGuiControls, "ratio_Road", 0.1,1);
+groundGUI.add(groundGuiControls, "show_Green");
+groundGUI.add(groundGuiControls, "show_Path");
+groundGUI.add(groundGuiControls, "show_Road");
+groundGUI.add(groundGuiControls, "show_Only_Ground");
+
+//building gui controls
+var bldgGuiControls=new function(){
+  this.evacuation_density=0.05;
+  this.res_FSR=0.3;
+  this.comm_FSR=0.3;
+  this.office_FSR=0.3;
+  this.min_Ht=3;
+  this.mid_Ht=7;
+  this.max_Ht=20;
   this.show_Evacuation=true;
   this.show_Residences=true;
   this.show_Commercial=true;
   this.show_Office=true;
-  
-  this.show_Information=false;
 }
-datgui.add(guiControls, "numL", 1, 5);
-datgui.add(guiControls, "numH", 1, 5);
-datgui.add(guiControls, "gridL", 1, 5);
-datgui.add(guiControls, "gridH", 1, 5);
+var buildingGUI=datgui.addFolder('bldgGuiControls');
+buildingGUI.add(bldgGuiControls, "evacuation_density", .001, 0.1);
+buildingGUI.add(bldgGuiControls, "res_FSR", 0.1,3);
+buildingGUI.add(bldgGuiControls, "comm_FSR", 0.1,3);
+buildingGUI.add(bldgGuiControls, "office_FSR", 0.1,3);
+buildingGUI.add(bldgGuiControls, "min_Ht",1,5);
+buildingGUI.add(bldgGuiControls, "mid_Ht",5,12);
+buildingGUI.add(bldgGuiControls, "max_Ht",12,25);
+buildingGUI.add(bldgGuiControls, "show_Evacuation");
+buildingGUI.add(bldgGuiControls, "show_Residences");
+buildingGUI.add(bldgGuiControls, "show_Commercial");
+buildingGUI.add(bldgGuiControls, "show_Office");
 
-datgui.add(guiControls, "evacuation_density", .001, 0.1);
-
-
-datgui.add(guiControls, "ratio_Road", 0.1,1);
-datgui.add(guiControls, "ratio_Path", 0.1,1);
-datgui.add(guiControls, "ratio_Green", 0.1,1);
-
-datgui.add(guiControls, "FSRRes", 0.1,3);
-datgui.add(guiControls, "FSRComm", 0.1,3);
-datgui.add(guiControls, "FSROffice", 0.1,3);
-
-datgui.add(guiControls, "minHt",1,5);
-datgui.add(guiControls, "midHt",5,12);
-datgui.add(guiControls, "maxHt",12,25);
-
-datgui.add(guiControls, "autoLoop");
-
-datgui.add(guiControls, "show_OnlyGround");
-
-datgui.add(guiControls, "show_Green");
-datgui.add(guiControls, "show_Road");
-datgui.add(guiControls, "show_Path");
-
-datgui.add(guiControls, "show_Evacuation");
-datgui.add(guiControls, "show_Residences");
-datgui.add(guiControls, "show_Commercial");
-datgui.add(guiControls, "show_Office");
-datgui.add(guiControls, "show_Information");
-
-
-var customContainer = document.getElementById('moveGUI');
-customContainer.appendChild(datgui.domElement);
-*/
-
-
-/*
-
-var guiControls=new function(){
-  this.numL=2;
-  this.numH=2;
-  this.gridL=2.5;
-  this.gridH=2.5;
-    
-  this.ratio_Road=0.25;
-  this.ratio_Path=0.50;
-  this.ratio_Green=0.25;
-  
-  this.evacuation_density=0.05;
-  this.FSRRes=0.3;
-  this.FSRComm=0.3;
-  this.FSROffice=0.3;
-  
-  this.minHt=3;
-  this.midHt=7;
-  this.maxHt=20;
-  
-  this.ratio_Res_Comm=0.3;
-  this.ratio_Comm_Off=0.3;
-  this.ratio_Off_Res=0.3;
-  
-  this.autoLoop=false;
-  
-  this.show_OnlyGround=false;
-  
-  this.show_Road=true;
-  this.show_Green=true;
-  this.show_Path=true;
-  
-  this.show_Evacuation=true;
-  this.show_Residences=true;
-  this.show_Commercial=true;
-  this.show_Office=true;
-  
-  this.show_Information=false;
+// main functions about the generation
+var genGuiControls=new function(){
+  this.AUTOLOOP=false;
+  this.show_Information=false;  
 }
+var genGUI=datgui.addFolder('genGuiControls');
+genGUI.add(genGuiControls, "AUTOLOOP");
+genGUI.add(genGuiControls, "show_Information");
 */
