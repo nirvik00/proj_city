a1d7297db6ace507e5950ec246b0d25d42a2c469
nirvik00 <nirviksaha@gatech.edu>
2019-02-13 17:52:05 -0500

    update file restructuring

--- public/js/shortestPathAlg.js
+++ public/js/shortestPathAlg.js
@@ -23,7 +23,7 @@ function getAllEdgesOfNode(node, edges){
        for(var i=0; i<neighbours.length; i++){
               ids.push(neighbours[i].id);
        }
-       console.log("\n\n\n\nneighbours id = "+ ids);
+       //console.log("\n\n\n\nneighbours id = "+ ids);
        return neighbours;
 }
 
@@ -47,16 +47,15 @@ function extractMinHeap(neighbours,nodeHeap){
               }
        }
        try{
-              console.log("min id = " + node.id);
+              //console.log("min id = " + node.id);
        }catch(err){
+              console.log("error in extract min heap function");
               console.log(err);
               console.log(nodeHeap);
        }
        
        return node;
 }
-
-
 
 function spt(edgeArr, nodeHeap, source, current){
        console.log("\n\n\nstart spt : " + source);
@@ -86,132 +85,148 @@ function spt(edgeArr, nodeHeap, source, current){
        console.log(resultNodeHeap);
 }
 
+// initiate edge weights - green network
+function initGreenEdges(){
+       for (var i = 0; i < networkEdgesArr.length; i++) {
+              var e = networkEdgesArr[i];
+              e.updateCost();
+              e.updateType();
+       }
+       
+       for(var i=0; i<networkNodesArr.length; i++){
+              networkNodesArr[i].id=i;
+       }
+       
+       var sortedNetworkEdges = new Array();  //sort all edges by weight
+       var sortable = new Array();
+       for (var i = 0; i < networkEdgesArr.length; i++) {
+              sortable.push([networkEdgesArr[i], networkEdgesArr[i].cost]);
+       }
+       sortable.sort(function(a, b) {
+              return a[1] - b[1];
+       });
+       networkEdgesArr = Array();
+       for (var i = 0; i < sortable.length; i++) {
+              networkEdgesArr.push(sortable[i][0]);
+       }
+       sortable = [];// end of sorting
+}
 
+// make sure nodes in array are correct - getPt()
+function getNodeHeap(){
+       var nodeHeap=[];
+       for (var i = 0; i < networkNodesArr.length; i++) {
+              try{
+              var p=networkNodesArr[i].getPt()
+              nodeHeap.push(networkNodesArr[i]);
+              }catch(err){
+              }          
+         }
+       return nodeHeap;
+}
 
 // shortest path algorithm - epsilon greedy
 function findMinCost() {
-  for (var i = 0; i < networkEdgesArr.length; i++) {
-    var e = networkEdgesArr[i];
-    e.updateCost();
-    e.updateType();
-  }
-
-  for(var i=0; i<networkNodesArr.length; i++){
-         networkNodesArr[i].id=i;
-  }
-
-  var sortedNetworkEdges = new Array();  //sort all edges by weight
-  var sortable = new Array();
-  for (var i = 0; i < networkEdgesArr.length; i++) {
-    sortable.push([networkEdgesArr[i], networkEdgesArr[i].cost]);
-  }
-  sortable.sort(function(a, b) {
-    return a[1] - b[1];
-  });
-  networkEdgesArr = Array();
-  for (var i = 0; i < sortable.length; i++) {
-    networkEdgesArr.push(sortable[i][0]);
-  }
-  sortable = [];// end of sorting
-  
-
-  
-  //get all valis nodes - point
-  var nodeHeap =[];  
-  for (var i = 0; i < networkNodesArr.length; i++) {
-       try{
-       var p=networkNodesArr[i].getPt()
-       nodeHeap.push(networkNodesArr[i]);
-       }catch(err){
-       }          
-  }
-
-
-  var source;
-  for(var i=0; i<nodeHeap.length; i++){
-         if(nodeHeap[i].getType()=="res"){
-                source=nodeHeap[i];
-                break;
-         }
-  }
-
-  console.log("\n\n\ninit to start spt : ");
-  console.log(source);
-
-
-  source.dist=0;
-  source.parent=null;
-  nodeHeap.splice(0,1);
-  var resultNodeHeap=[];
-  resultNodeHeap.push(source);
-  
-  var k=0; 
-  while(nodeHeap.length>0){
-         var neighbours=getAllEdgesOfNode(source, networkEdgesArr);
-         source=extractMinHeap(neighbours, nodeHeap);
-         try{
-              for(var i=0; i<nodeHeap.length; i++){     //remove the source from nodeheap
-                     if(utilDi(source.getPt(),nodeHeap[i].getPt())< 0.1){
-                            nodeHeap.splice(i,1);
-                            break;
-                     }
+       initGreenEdges();              //sort all edges by weight- for convenience
+       var nodeHeap = getNodeHeap();      //get all valis nodes - point
+       var source;                 //get source node: first res in node heap
+       for(var i=0; i<nodeHeap.length; i++){
+              if(nodeHeap[i].getType()=="res"){
+                     source=nodeHeap[i];
+                     break;
               }
-         }catch(err){
-                console.log("error found");
-                console.log(nodeHeap);
-                break;
-         }
-         
-         resultNodeHeap.push(source);
-         console.log("\nnew source:");
-         console.log(source);
-         console.log("\nremaining node heap: ");
-         console.log(nodeHeap);
-         k++;
-  }      
+       }
 
-  console.log("\n\n\nsolution: ");
-  console.log(resultNodeHeap);
-  
-  //find the shortest path between the first node and last node
+       source.dist=0;                     //initialize alg with source dist=0
+       source.parent=null;                //source parent is null
+       nodeHeap.splice(0,1);              //remove source from nodeheap
+       var resultNodeHeap=[];             //init result nodeheap - to store nodes
+       resultNodeHeap.push(source);       //first element is the source
 
-//get all nodes of "res" type
+       //console.log("\n\n\ninit to start spt : ");
+       //console.log(source);
 
-var reqResNodes=[];
-for (var i = 0; i < networkNodesArr.length; i++) {
-       if(networkNodesArr[i].getType()==="res"){
+       //djikstra algorithm to find min dist of all nodes from source 
+       //recursive algorithm:
+       //1. find all neighbours and update dist
+       //2. extract min from the heap
+       //loop until result heap is as big as initial nodeHeap
+       var k=0; 
+       while(nodeHeap.length>0){
+              var neighbours=getAllEdgesOfNode(source, networkEdgesArr);
+              source=extractMinHeap(neighbours, nodeHeap);
               try{
-                     var p=networkNodesArr[i].getPt()
-                     reqResNodes.push(networkNodesArr[i]);
+                     for(var i=0; i<nodeHeap.length; i++){     //remove the source from nodeheap
+                            if(utilDi(source.getPt(),nodeHeap[i].getPt())< 0.1){
+                                   nodeHeap.splice(i,1);
+                                   break;
+                            }
+                     }
+              }catch(err){
+                     console.log("error found");
+                     console.log(nodeHeap);
+                     break;
+              }
+              
+              resultNodeHeap.push(source);
+              console.log("\nnew source:");
+              console.log(source);
+              console.log("\nremaining node heap: ");
+              console.log(nodeHeap);
+              k++;
+       }      
+
+       //console.log("\n\n\nsolution: ");
+       //console.log(resultNodeHeap);
+       
+       
+       //get all nodes of "res" type
+       //each res type will be a sink; make an array of sinks
+       //the required spine will be a summation of all the paths to source
+       var reqResNodes=[];
+       for (var i = 0; i < networkNodesArr.length; i++) {
+              if(networkNodesArr[i].getType()==="res"){
+                     try{
+                            var p=networkNodesArr[i].getPt()
+                            reqResNodes.push(networkNodesArr[i]);
                      }catch(err){
-                
+                            //error in getPT();
                      }   
+              }
        }
-}
-var source;
-var minDi=10000000;
-var req;
-for(var i=0; i<reqResNodes.length; i++){
-       var node=reqResNodes[i];
-       if(node.dist<minDi && node.getType()=="res"){
-              minDi=node.dist;
-              source=node;
+
+       //once again find the source
+       //this is the node with min dist
+       var source;
+       var minDi=10000000;
+       var req;
+       for(var i=0; i<reqResNodes.length; i++){
+              var node=reqResNodes[i];
+              if(node.dist<minDi && node.getType()=="res"){
+                     minDi=node.dist;
+                     source=node;
+              }
        }
+
+       //get all paths from each element of the sink array
+       //set each edge to green
+       //getPath is recursive function:
+       //1.start with the sink
+       //2.find parent and add to array
+       //3.loop until parent is source
+
+       for(var i=0; i<reqResNodes.length; i++){
+              var sink=reqResNodes[i];
+              var tmpArr=[];
+              tmpArr=getPath(source, sink, resultNodeHeap, networkEdgesArr, tmpArr);
+              setEdgeToType(tmpArr,"green");
+       }
+
+       //finally set the edges to green
+       genNetworkGeometry();
 }
 
-for(var i=0; i<reqResNodes.length; i++){
-       var sink=reqResNodes[i];
-       var tmpArr=[];
-       tmpArr=getPath(source, sink, resultNodeHeap, networkEdgesArr, tmpArr);
-       setEdgeToGreen(tmpArr);
-}
-
-
-genNetworkGeometry();
-
-}
-
-function setEdgeToGreen(tmpArr){
+function setEdgeToType(tmpArr, type){
        for(var i=0; i<tmpArr.length; i++){
               var p=tmpArr[i].getNode0().getPt();
               var q=tmpArr[i].getNode1().getPt();
@@ -219,11 +234,11 @@ function setEdgeToGreen(tmpArr){
                      var r=networkEdgesArr[j].getNode0().getPt();
                      var s=networkEdgesArr[j].getNode1().getPt();
                      if(utilDi(p,r)<0.1 && utilDi(q,s)<0.1){
-                            networkEdgesArr[j].setType("green");
+                            networkEdgesArr[j].setType(type);
                             break;
                      }
                      if(utilDi(p,s)<0.1 && utilDi(q,r)<0.1){
-                            networkEdgesArr[j].setType("green");
+                            networkEdgesArr[j].setType(type);
                             break;
                      }
               }
