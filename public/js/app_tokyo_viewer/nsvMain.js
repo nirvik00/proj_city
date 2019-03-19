c9ae3f58a4e2aa0e950018d8110f22c3acb8e998
nirvik00 <nirviksaha@gatech.edu>
2019-03-19 03:40:57 -0400

    to complete db routes are set

--- public/js/app_tokyo_viewer/nsvMain.js
+++ public/js/app_tokyo_viewer/nsvMain.js
@@ -2,7 +2,7 @@ var initGeometry=function(allobjs){
     for(var i=0; i<allobjs.length; i++){
         var arr=allobjs[i].data.split(";");
         for(var j=0; j<arr.length; j++){
-            //console.log(arr[j]);
+            console.log(arr[j]);
         }        
     }
 }
